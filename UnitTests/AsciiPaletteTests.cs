e062404056116a99a5586125799c032e3cf10507
dmitry-abramov <abramov.dmitry92@gmail.com>
2015-08-29 20:46:32 +0300

    added NUnit

---
+++ UnitTests/AsciiPaletteTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+using SymbolizeIT;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class AsciiPaletteTests
+    {
+        [Test]
+        public void CreatePalette()
+        {
+            var characters = new List<char> { 'a', 'b', 'c' };
+            var palette = new AsciiPalette(characters, true);
+
+            CollectionAssert.AreEqual(characters, palette);
+            Assert.IsTrue(palette.IsBrightnessSorted);
+        }
+    }
+}
